61f4e7661df5457a8606e70d079568eb985aeba2
Md. Kamrul Hasan <1703081kamrul@gmail.com>
2025-05-25 01:09:18 +0600

    check upload to git2

--- Program.cs
+++ Program.cs
@@ -10,5 +10,5 @@ catch (Exception ex)
 {
     Console.WriteLine($"Error: {ex.Message}");
     Console.WriteLine("Example usage: NonTransitiveDiceGame.exe 1,2,3,4,5,6 2,2,4,4,9,9 3,3,5,5,7,7");
-    ////hello checking push...
+    //hello checking push...
 }
